e69072cec470f24d575afcb7ea39b1f54e3caf80
vulture787 <91385513+vulture787@users.noreply.github.com>
2021-10-30 11:11:17 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -37,3 +37,28 @@ public class day2 {
 
         return index;
     }
+
+    public static int findindex2(int[] nums, int target) {
+
+        int index = -1;
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target)
+                index = mid;
+            if (nums[mid] <= target)
+                start = mid + 1;
+            else
+                end = mid - 1;
+
+
+        }
+        return index;
+
+
+    }
+}
